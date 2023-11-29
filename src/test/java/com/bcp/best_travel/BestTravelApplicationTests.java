689b40addf07d9694511cbdfe90d989e945e9de3
valerian <jhingx1@bcp.com.pe>
2023-11-29 06:39:42 -0500

    Estructura basica

---
+++ src/test/java/com/bcp/best_travel/BestTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.best_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
